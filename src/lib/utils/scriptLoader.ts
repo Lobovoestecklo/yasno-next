3fc04f60e8e2ee3627308d6e7ad9617aa093c713
Sergei Khmylov <sergei@khmylov.me>
2025-02-04 09:07:53 +0100

    remove require

--- src/lib/utils/scriptLoader.ts
+++ src/lib/utils/scriptLoader.ts
@@ -1,9 +1,10 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
 export function loadScriptFromFile(filePath: string): string {
     try {
-      const fs = require('fs');
-      const path = require('path');
-      const absolutePath = path.resolve(filePath);
-      return fs.readFileSync(absolutePath, { encoding: 'utf-8' });
+      const absolutePath = resolve(filePath);
+      return readFileSync(absolutePath, { encoding: 'utf-8' });
     } catch (error) {
       console.error(`Error loading the file: ${error}`);
       return '';
